7f8b9c3572eaafe4688dfb7ae8c225f502200aa6
Inderpal Singh <inderpal2406@gmail.com>
2021-09-13 10:41:52 +0530

    Add code for allow-all-outgoing-sg & allow-http-https-sg.

--- terraform-files/security-group.tf
+++ terraform-files/security-group.tf
@@ -1,5 +1,27 @@
 # This file will define security groups being used for our EC2 instances.
 
+# Allow all outgoing traffic.
+
+resource "aws_security_group" "allow-all-outgoing-sg" {
+  name		= "allow-all-outgoing-sg" 
+  description	= "Allow all outgoing traffic from the instance."
+  vpc_id	= aws_vpc.main-vpc.id
+
+  egress {
+    description = "Allow all outgoing traffic from the instance."
+    from_port	= 0
+    to_port	= 0
+    protocol	= "-1"
+    cidr_blocks	= ["0.0.0.0/0"]
+  }
+
+  tags		= {
+    Name	= "allow-all-outgoing-sg"
+    ManagedBy	= "terraform"
+    Owner	= "Inderpal Singh"
+  }
+}
+
 # Allow SSH security group.
 
 resource "aws_security_group" "allow-ssh-sg" {
@@ -12,14 +34,6 @@ resource "aws_security_group" "allow-ssh-sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    description = "Allow all outbound traffic."
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
@@ -45,18 +59,40 @@ resource "aws_security_group" "saltmaster-sg" {
     cidr_blocks = ["10.0.1.0/24"]
   }
 
-  egress {
-    description = "Allow all outbound traffic."
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
   tags = {
     Name      = "saltmaster-sg"
     ManagedBy = "terraform"
     Owner     = "Inderpal Singh"
+  }
+}
+
+# Security group to allow incoming HTTP/HTTPS (web) traffic.
+
+resource "aws_security_group" "allow-http-https-sg" {
+  name		= "allow-http-https-sg"
+  description	= "Allow incoming HTTP/HTTPS (web) traffic."
+  vpc_id	= aws_vpc.main-vpc.id
+
+  ingress {
+    description = "Allow incoming HTTP (web) traffic."
+    from_port	= 80
+    to_port	= 80
+    protocol	= "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description	= "Allow incmoing HTTPS (web) traffic."
+    from_port	= 443
+    to_port	= 443
+    protocol	= "tcp"
+    cidr_blocks	= ["0.0.0.0/0"]
+  }
+
+  tags		= {
+    Name	= "allow-http-https-sg"
+    ManagedBy	= "terraform"
+    Owner	= "Inderpal Singh"
   }
 }
 
