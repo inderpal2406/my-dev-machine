da5c69722f268630802583c01ed73296fc061f11
Inderpal Singh <inderpal2406@gmail.com>
2021-09-13 10:11:15 +0530

    Add code to provision staging and prod web and DB servers.

--- terraform-files/ec2-instance.tf
+++ terraform-files/ec2-instance.tf
@@ -97,3 +97,71 @@ resource "aws_instance" "dev-db-01" {
   user_data = file("bash_scripts/configure_saltminion_dev-db-01.sh")
 }
 
+# Staging WEB server.
+
+resource "aws_instance" "staging-web-01" {
+  ami			 = "ami-0292c68e0ac530398"
+  instance_type		 = "t2.micro"
+  key_name		 = aws_key_pair.linux-ssh-key.id
+  subnet_id		 = aws_subnet.main-vpc-public-subnet.id
+  vpc_security_group_ids = [aws_security_group.allow-ssh-sg.id]
+  private_ip		 = "10.0.1.9"
+  tags			 = {
+    Name		 = "staging-web-01"
+    ManagedBy		 = "terraform"
+    Owner		 = "Inderpal Singh"
+  }
+  user_data		 = file("bash_scripts/configure_saltminion_staging-web-01.sh")
+}
+
+# Staging DB server.
+
+resource "aws_instance" "staging-db-01" {
+  ami			 = "ami-0292c68e0ac530398"
+  instance_type		 = "t2.micro"
+  key_name		 = aws_key_pair.linux-ssh-key.id
+  subnet_id		 = aws_subnet.main-vpc-public-subnet.id
+  vpc_security_group_ids = [aws_security_group.allow-ssh-sg.id]
+  private_ip		 = "10.0.1.10"
+  tags			 = {
+    Name		 = "staging-db-01"
+    ManagedBy		 = "terraform"
+    Owner		 = "Inderpal Singh"
+  }
+  user_data		 = file("bash_scripts/configure_saltminion_staging-db-01.sh")
+}
+
+# Production WEB server
+
+resource "aws_instance" "prod-web-01" {
+  ami			 = "ami-0292c68e0ac530398"
+  instance_type		 = "t2.micro"
+  key_name		 = aws_key_pair.linux-ssh-key.id
+  subnet_id		 = aws_subnet.main-vpc-public-subnet.id
+  vpc_security_group_ids = [aws_security_group.allow-ssh-sg.id]
+  private_ip		 = "10.0.1.11"
+  tags			 = {
+    Name		 = "prod-web-01"
+    ManagedBy		 = "terraform"
+    Owner		 = "Inderpal Singh"
+  }
+  user_data		 = file("bash_scripts/configure_saltminion_prod-web-01.sh")
+}
+
+# Production DB server
+
+resource "aws_instance" "prod-db-01" {
+  ami			 = "ami-0292c68e0ac530398"
+  instance_type		 = "t2.micro"
+  key_name		 = aws_key_pair.linux-ssh-key.id
+  subnet_id		 = aws_subnet.main-vpc-public-subnet.id
+  vpc_security_group_ids = [aws_security_group.allow-ssh-sg.id]
+  private_ip		 = "10.0.1.12"
+  tags			 = {
+    Name		 = "prod-db-01"
+    ManagedBy		 = "terraform"
+    Owner		 = "Inderpal Singh"
+  }
+  user_data		 = file("bash_scripts/configure_saltminion_prod-db-01.sh")
+}
+
